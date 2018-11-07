b0d8a45b6043a51029fd0df4a8a2996757efb40e
Robin Goetz <tug29225@temple.edu>
2018-11-07 12:34:21 -0500

    app widget

--- landlord/js/landlord_dashboard.js
+++ landlord/js/landlord_dashboard.js
@@ -1,3 +1,167 @@
-$(function(){
-    
+$(function () {
+    var submitted_applications = new Array();
+    var rejected_applications = new Array();
+    var accepted_applications = new Array();
+
+    var open_index = 0;
+    var acc_index = 0;
+    var rej_index = 0;
+
+
+    get_applications(submitted_applications, rejected_applications, accepted_applications);
+
+    // add event listener to application widget navigation
+    $(".next_app").click(function (e) {
+        e.preventDefault();
+
+
+        if ($(e.currentTarget).attr('data') == 'open') {
+            if (open_index < submitted_applications.length - 1) {
+                open_index++;
+                update_app_list('open-apps', submitted_applications[open_index]);
+            }
+
+
+        }
+        if ($(e.currentTarget).attr('data') == 'rejected') {
+            if (rej_index < rejected_applications.length - 1) {
+                rej_index++;
+                update_app_list('rejected-apps', rejected_applications[rej_index]);
+            }
+        }
+        if ($(e.currentTarget).attr('data') == 'accepted') {
+            if (acc_index < accepted_applications.length - 1) {
+                acc_index++;
+                update_app_list('accepted-apps', accepted_applications[acc_index]);
+            }
+        }
+    });
+
+    // add event listener to application widget navigation
+    $(".last_app").click(function (e) {
+        e.preventDefault();
+
+
+        if ($(e.currentTarget).attr('data') == 'open') {
+            if (open_index > 0) {
+                open_index--;
+                update_app_list('open-apps', submitted_applications[open_index]);
+            }
+        }
+        if ($(e.currentTarget).attr('data') == 'rejected') {
+            if (rej_index > 0) {
+                rej_index--;
+                update_app_list('rejected-apps', rejected_applications[rej_index]);
+            }
+        }
+        if ($(e.currentTarget).attr('data') == 'accepted') {
+            if (acc_index > 0) {
+                acc_index--;
+                update_app_list('accepted-apps', accepted_applications[acc_index]);
+            }
+        }
+    });
+
+    //add event listner to application button accept
+    $("#open_app_accept").click(function (e) {
+        e.preventDefault();
+        console.log($(this).data('app_id'));
+    })
+
+    $("#open_app_reject").click(function (e) {
+        e.preventDefault();
+        console.log($(this).data('app_id'));
+    })
+
+    $("#refresh_apps").click(function (e) {
+        e.preventDefault();
+        get_applications(submitted_applications, rejected_applications, accepted_applications);
+    })
 });
+
+var update_app_list = function (type, application) {
+    var $body = $('.' + type).find('.app_body');
+    $body.find('h1').html(application.street);
+    console.log(application);
+    var message_text = application.renter_message ? application.renter_message : "This applicant did not leave a message.";
+
+    $body.find('.message').html(message_text);
+
+    $body.find('.info').html(
+        "<div class='d-flex justify-content-between'>" +
+        "<div>MOVE IN: " + application.move_in_date + "</div>" +
+        "<div>MOVE OUT: " + ((application.move_out_date) ? application.move_out_date : "unknown ") + " </div>" +
+        "</div>" +
+        "<div>BY: " + application.firstname + " " + application.lastname + "</div>");
+
+    if (type == 'open-apps') {
+        console.log('setting app_id', application.rental_application_id);
+        $('#open_app_accept').data('app_id', application.rental_application_id);
+        $('#open_app_reject').data('app_id', application.rental_application_id);
+    }
+
+}
+
+var build_app_list_html = function (data_arr) {
+    var html = "";
+    $.each(data_arr, function (indexInArray, element) {
+        html += build_single_app_html(element, indexInArray);
+    });
+    // console.log(html);
+    if (html.length == 0) {
+        html = build_no_app_html();
+    }
+    return html
+}
+
+var get_applications = function (submitted_applications, rejected_applications, accepted_applications) {
+
+    submitted_applications = new Array();
+    rejected_applications = new Array();
+    accepted_applications = new Array();
+
+    url = base_path + "services/index.php?type=get_application_data";
+    // console.log(url);
+    $('#app_spinner').removeClass('d-none');
+    $('#app_tab').addClass('d-none');
+    $.getJSON(url,
+        function (data, textStatus, jqXHR) {
+            // console.log(data);
+            $.each(data, function (indexInArray, element) {
+                switch (element.app_status_id) {
+                    case "1":
+                        submitted_applications.push(element);
+                        break;
+                    case "2":
+                        rejected_applications.push(element);
+                        break;
+                    case "3":
+                        accepted_applications.push(element);
+                        break;
+                }
+            });
+
+            $('#app_spinner').addClass('d-none');
+            $('#app_tab').removeClass('d-none');
+            display_array(submitted_applications, 'open-apps');
+            display_array(rejected_applications, 'rejected-apps');
+            display_array(accepted_applications, 'accepted-apps');
+        }
+    );
+}
+
+var display_array = function (array, fieldname) {
+    if (array.length > 0) {
+        console.log(array.length);
+        $('.' + fieldname).removeClass('d-none');
+        $('.' + fieldname + '-empty').addClass('d-none');
+        update_app_list(fieldname, array[0]);
+
+    }
+    if (array.length <= 0) {
+        console.log(array.length);
+        $('.' + fieldname).addClass('d-none');
+        $('.' + fieldname + '-empty').removeClass('d-none');
+
+    }
+}
--- landlord/landlord_dashboard.php
+++ landlord/landlord_dashboard.php
@@ -133,29 +133,97 @@
                 <div class='db_heading'>APPLICATIONS</div>
                 <div class="p-2 col-md-2 d-flex justify-content-center">
                     </div>
-                <i class="prop__action__icon fas fa-sync"></i>
+                <i id='refresh_apps' class="prop__action__icon fas fa-sync"></i>
                 </div>
 
               <ul class="nav nav-tabs" id="myTab" role="tablist">
                     <li class="nav-item">
-                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#open-apps" role="tab" aria-controls="open-apps" aria-selected="true">Open</a>
+                        <a class="nav-link active" id="open-tab" data-toggle="tab" href="#open-apps" role="tab" aria-controls="open-apps" aria-selected="true">Open</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#approved-apps" role="tab" aria-controls="approved-apps" aria-selected="false">Approved</a>
+                        <a class="nav-link" id="approved-tab" data-toggle="tab" href="#approved-apps" role="tab" aria-controls="approved-apps" aria-selected="false">Approved</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" id="contact-tab" data-toggle="tab" href="#rejected-apps" role="tab" aria-controls="rejected-apps" aria-selected="false">Rejected</a>
+                        <a class="nav-link" id="rejected-tab" data-toggle="tab" href="#rejected-apps" role="tab" aria-controls="rejected-apps" aria-selected="false">Rejected</a>
                     </li>
                 </ul>
-                    <div class="tab-content" id="app_tab">
-                        <div class="tab-pane fade show active " id="open-apps" role="tabpanel" aria-labelledby="home-tab">
-                                        
+                    <div id='app_spinner' class='spinner'></div>
+                    <div class="tab-content d-none" id="app_tab">
+                        <div class="tab-pane fade show active " id="open-apps" role="tabpanel" aria-labelledby="open-tab">
+                            <div class='open-apps apps_container container d-none'>
+                                <div class='row'>
+                                    <div class='col-12'>
+                                        <div class='app_navigation open'>
+                                            <a class='app_navigation_arrow last_app' data='open' href=''><i class='fas fa-angle-left'></i></a>
+                                            <a class='app_navigation_arrow next_app' data='open' href=''><i class='fas fa-angle-right'></i></a>
+                                        </div>
+                                        <div class='app_body'>
+                                            <div class='heading'>OPEN APPLICATION</div>
+                                                <h1>1604 Willington Street, 19121 Philadelphia PA</h1>
+                                                <div class='info'>RECEIVED ON 10/11/2018 BY JESSIE JAMES</div>
+                                                <div class=message>
+                                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, nulla molestiae et ratione magnam tempora sequi 
+                                                    labore pariatur illo maxime sit quae suscipit beatae non quis omnis perferendis cumque odio?
+                                                </div>
+                                        </div>
+                                        <div class='app_buttons'>
+                                            <button id='open_app_accept' class='btn btn-primary red'><i class='fas fa-check'></i></button>
+                                            <button id='open_app_reject' class='btn btn-primary red'><i class='fas fa-times'></i></button>
+                                        </div>
+                                    </div>
+                                    </div>
+                            </div>
+                            <div class='apps-empty open-apps-empty'><i class='fas fa-info icon_red_40'></i><div>No Applications to be displayed...</div></div>
                         </div>
-                        <div class="tab-pane fade " id="approved-apps" role="tabpanel" aria-labelledby="profile-tab">
-                                
+                        <div class="tab-pane fade " id="approved-apps" role="tabpanel" aria-labelledby="approved-tab">
+                        <div class='approved-apps apps_container container d-none'>
+                                <div class='row'>
+                                    <div class='col-12'>
+                                        <div class='app_navigation'>
+                                            <a class='app_navigation_arrow last_app' data='approved' href=''><i class='fas fa-angle-left'></i></a>
+                                            <a class='app_navigation_arrow next_app' data='approved' href=''><i class='fas fa-angle-right'></i></a>
+                                        </div>
+                                        <div class='app_body approved'>
+                                            <div class='heading'>APPROVED APPLICATION</div>
+                                                <h1>1604 Willington Street, 19121 Philadelphia PA</h1>
+                                                <div class='info'>RECEIVED ON 10/11/2018 BY JESSIE JAMES</div>
+                                                <div class=message>
+                                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, nulla molestiae et ratione magnam tempora sequi 
+                                                    labore pariatur illo maxime sit quae suscipit beatae non quis omnis perferendis cumque odio?
+                                                </div>
+                                        </div>
+                                        <div class='app_buttons'>
+                                            <!-- <button class='btn btn-primary'><i class='fas fa-check'></i></button>
+                                            <button class='btn btn-primary'><i class='fas fa-times'></i></button> -->
+                                        </div>
+                                    </div>
+                                    </div>
+                            </div>
+                            <div class='apps-empty approved-apps-empty'><i class='fas fa-info icon_red_40'></i><div>No Applications to be displayed...</div></div>
                         </div>
-                        <div class="tab-pane fade " id="rejected-apps" role="tabpanel" aria-labelledby="contact-tab">   
-                             
+                        <div class="tab-pane fade " id="rejected-apps" role="tabpanel" aria-labelledby="rejected-tab">   
+                        <div class='rejected-apps apps_container container d-none'>
+                                <div class='row'>
+                                    <div class='col-12'>
+                                        <div class='app_navigation'>
+                                            <a class='app_navigation_arrow last_app' data='rejected' href=''><i class='fas fa-angle-left'></i></a>
+                                            <a class='app_navigation_arrow next_app' data='rejected' href=''><i class='fas fa-angle-right'></i></a>
+                                        </div>
+                                        <div class='app_body rejected'>
+                                            <div class='heading'>REJECTED APPLICATION</div>
+                                                <h1>1604 Willington Street, 19121 Philadelphia PA</h1>
+                                                <div class='info'>RECEIVED ON 10/11/2018 BY JESSIE JAMES</div>
+                                                <div class=message>
+                                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, nulla molestiae et ratione magnam tempora sequi 
+                                                    labore pariatur illo maxime sit quae suscipit beatae non quis omnis perferendis cumque odio?
+                                                </div>
+                                        </div>
+                                        <div class='app_buttons'>
+                                        </div>
+                                    </div>
+                                    </div>
+                            </div>
+                            <div class='apps-empty rejected-apps-empty'><i class='fas fa-info icon_red_40'></i><div>No Applications to be displayed...</div></div>
                     </div>
                     </div>
 
